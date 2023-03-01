b95036bd76f65a53997569c30b68960e185f1624
sjkim04 <sungntung@gmail.com>
2023-03-01 05:29:24 +0000

    Initial commit

---
+++ commands/ping.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		const sent_locales = {
+			en: 'Pinging...',
+			ko: '핑하는 중입니다...',
+		};
+		const received_locales = {
+			en: 'Pong! Latency: ',
+			ko: '퐁! 핑 수치: ',
+		};
+		const sent = await interaction.reply ({ content: sent_locales[interaction.locale] ?? sent_locales['en'], fetchReply: true, ephemeral: true });
+		interaction.editReply(`${received_locales[interaction.locale] ?? received_locales['en']}${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+	},
+};

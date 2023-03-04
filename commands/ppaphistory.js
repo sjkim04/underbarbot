1ea02a4a659742a5c8ce34c72864ac02cb6dde77
sjk <sungntung@gmail.com>
2023-03-04 22:38:20 +0900

    feat: add ppap chance

--- commands/ppaphistory.js
+++ commands/ppaphistory.js
@@ -7,6 +7,6 @@ module.exports = {
 		.setDescription('historyappleapplepen'),
 	execute(interaction) {
 		const jsonObject = JSON.parse(fs.readFileSync('./ppap.json'));
-		interaction.reply(`Pen: ${jsonObject.pen}\nPineapple: ${jsonObject.pineapple}\nApple: ${jsonObject.apple}\nPPAP: ${jsonObject.ppap}`);
+		interaction.reply(`Pen: ${jsonObject.pen}\nPineapple: ${jsonObject.pineapple}\nApple: ${jsonObject.apple}\nPPAP: ${jsonObject.ppap}\n\n확률: ${Math.round(((jsonObject.pen + jsonObject.pineapple + jsonObject.apple + jsonObject.ppap) / jsonObject.ppap) * 1000) / 1000}`);
 	},
 };

acaeebcdc41d59f952065287059a1feb84c859a4
sjk <sungntung@gmail.com>
2023-03-05 19:25:56 +0900

    Remove ffmpeg, adjust to  webm, update ppap

--- musicQueue.js
+++ musicQueue.js
@@ -1,5 +1,5 @@
 const ytdl = require('ytdl-core');
-const { createAudioResource } = require('@discordjs/voice');
+const { createAudioResource, StreamType } = require('@discordjs/voice');
 
 const play = (client, guildId) => {
 	const queue = client.queue;
@@ -8,11 +8,9 @@ const play = (client, guildId) => {
 	const player = queue[guildId].player;
 	const resource = createAudioResource(ytdl(url, {
 		filter: 'audioonly',
+		format: 'webm',
 		quality: 'highestaudio',
-		dlChunkSize: 0,
-		bitrate: 128,
-		highWaterMark: 1 << 62,
-	}));
+	}), { inputType: StreamType.WebmOpus });
 	player.play(resource);
 };
 
@@ -20,14 +18,14 @@ const getNextResource = (client, guildId) => {
 	const queue = client.queue;
 	if (queue[guildId]) {
 		queue[guildId].playlist.shift();
-		if (queue[guildId].playlist.length == 0) {
+		if (queue[guildId].playlist.length === 0) {
 			delete queue[guildId];
 		}
 		else {
 			play(client, guildId);
 		}
-		client.queue = queue;
 	}
+
 };
 
 module.exports = { play, getNextResource };

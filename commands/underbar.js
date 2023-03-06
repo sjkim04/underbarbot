3fccf2ddc97df396f1dba9b09cf463b4f29e0813
sjk <sungntung@gmail.com>
2023-03-06 21:34:23 +0900

    Add nohup.out to ignore, change qual to r1

--- commands/underbar.js
+++ commands/underbar.js
@@ -13,14 +13,14 @@ module.exports = {
 		)
 		.addSubcommand(subcommand =>
 			subcommand
-				.setName('qual_ppl')
-				.setDescription('예선 채보 제출자 수를 확인합니다.'),
+				.setName('r1_ppl')
+				.setDescription('1라운드 채보 제출자 수를 확인합니다.'),
 		),
 	async execute(interaction) {
 		if (interaction.options.getSubcommand() === 'ping') {
 			interaction.reply({ content: '<@359113390469283842>', allowedMentions: { users: [] } });
 		}
-		else if (interaction.options.getSubcommand() === 'qual_ppl') {
+		else if (interaction.options.getSubcommand() === 'r1_ppl') {
 			await interaction.deferReply();
 			const api = axios.create({
 				baseURL: `https://sheets.googleapis.com/v4/spreadsheets/${config.underbarAPIsheet}`,
@@ -29,7 +29,7 @@ module.exports = {
 				},
 			});
 			const { data: { values } } = await api.get('values/\'예선 제출 관리\'!L3');
-			interaction.editReply(`${values[0]}명이 언더바게임 예선에 채보를 제출했습니다.`);
+			interaction.editReply(`${values[0]}명이 언더바게임 1라운드에 채보를 제출했습니다.`);
 		}
 	},
 };

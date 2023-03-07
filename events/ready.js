ea692ebb5388238f75f95227b3cc2720def9e69d
sjk <sungntung@gmail.com>
2023-03-07 18:53:57 +0900

    Fetch and cache all guild members on ready

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,9 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		const guild = client.guilds.cache.get('1079040702798893127');
+		guild.members.fetch();
+
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };

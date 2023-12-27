ca3667961de476779b7e007ea22087c195206ade
sjk <sungntung@gmail.com>
2023-12-28 00:06:35 +0900

    feat(underbar): debuff

--- src/commands/underbar.ts
+++ src/commands/underbar.ts
@@ -1,6 +1,17 @@
 import { UpdatedInteraction } from '..';
+import fs from 'node:fs';
 
-import { SlashCommandBuilder } from 'discord.js';
+import {
+    BaseGuildTextChannel,
+    ButtonBuilder,
+    CategoryChannel,
+    ChannelType,
+    ForumChannel,
+    GuildMemberRoleManager,
+    ModalBuilder,
+    SlashCommandBuilder,
+    TextInputBuilder,
+} from 'discord.js';
 //import axios from 'axios';
 //import config from '../config.json';
 
@@ -10,6 +21,18 @@ module.exports = {
         .setDescription('언더바게임에 관한 명령어입니다.')
         .addSubcommand((subcommand) =>
             subcommand.setName('ping').setDescription('언더바를 핑합니다.'),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('debuff')
+                .setDescription('디버프를 제출합니다.')
+                .addStringOption((option) =>
+                    option
+                        .setName('debuff')
+                        .setDescription('디버프를 입력하세요.')
+                        .setDescriptionLocalization('en-US', 'Please enter your debuff.')
+                        .setRequired(true),
+                ),
         ),
     //.addSubcommand(subcommand =>
     //	subcommand
@@ -22,6 +45,31 @@ module.exports = {
                 content: '<@359113390469283842>',
                 allowedMentions: { users: [] },
             });
+        } else if (interaction.options.getSubcommand() === 'debuff') {
+            if (interaction.member.roles instanceof GuildMemberRoleManager) {
+                if (
+                    !interaction.member.roles.cache.some(
+                        (role) => role.id === '1189537394966413322',
+                    )
+                )
+                    return;
+            } else if (!interaction.member.roles.includes('1189537394966413322')) return;
+
+            const debuff = interaction.options.getString('debuff', true).replaceAll('///', '\n');
+            const jsonObject = JSON.parse(fs.readFileSync('./debuff.json', 'utf-8'));
+            const action = jsonObject[interaction.user.id] ? '수정' : '등록';
+            const oldDebuff = jsonObject[interaction.user.id] || null;
+            jsonObject[interaction.user.id] = debuff;
+            fs.writeFileSync('./debuff.json', JSON.stringify(jsonObject));
+
+            const confCh = interaction.guild.channels.cache.get('1083010848034922616');
+            if (confCh instanceof CategoryChannel || confCh instanceof ForumChannel) return;
+            await confCh.send(
+                `디버프가 ${action}되었습니다!\n\n참가자: ${interaction.user}\n${
+                    action === '수정' ? `기존 디버프: ${oldDebuff}\n 수정 ` : ''
+                }디버프: **${debuff}**`,
+            );
+            return interaction.reply({ content: `디버프가 ${action}되었습니다.`, ephemeral: true });
         }
         //else if (interaction.options.getSubcommand() === 'qual_ppl') {
         //	await interaction.deferReply();

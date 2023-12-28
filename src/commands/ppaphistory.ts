508fc5e954805d8cfe00f1c59aa05aeb3fc4b9bd
sjk <sungntung@gmail.com>
2023-12-28 10:16:05 +0900

    chore: show ppapsp in ppaphistory

--- src/commands/ppaphistory.ts
+++ src/commands/ppaphistory.ts
@@ -11,33 +11,29 @@ module.exports = {
                 .setName('season')
                 .setDescription('whatseasonapplepen')
                 .setRequired(false)
-                .addChoices(
-                    { name: 'Season 2', value: 's2' },
-                    { name: 'Season 1', value: 's1' },
-                ),
+                .addChoices({ name: 'Season 2', value: 's2' }, { name: 'Season 1', value: 's1' }),
         ),
     execute(interaction: UpdatedInteraction) {
         const option = interaction.options.getString('season', false) ?? 's2';
-        const rootJsonObject = JSON.parse(
-            fs.readFileSync('./ppap.json', 'utf-8'),
-        );
+        const rootJsonObject = JSON.parse(fs.readFileSync('./ppap.json', 'utf-8'));
         const jsonObject = rootJsonObject[option];
 
         interaction.reply(
-            `${option.toUpperCase()}의 ppap 성적:\nPen: ${
-                jsonObject.pen
-            }\nPineapple: ${jsonObject.pineapple}\nApple: ${
-                jsonObject.apple
-            }\nPPAP: ${jsonObject.ppap}\n\n확률: 1/${
+            `${option.toUpperCase()}의 ppap 성적:\nPen: ${jsonObject.pen}\nPineapple: ${
+                jsonObject.pineapple
+            }\nApple: ${jsonObject.apple}\nPPAP: ${jsonObject.ppap}${
+                option === 's2' && jsonObject.ppapsp > 0 ? `\nPPAP SP: ${jsonObject.ppapsp}` : ''
+            }\n\n확률: ${
                 Math.round(
-                    ((jsonObject.pen +
-                        jsonObject.pineapple +
-                        jsonObject.apple +
-                        jsonObject.ppap) /
-                        jsonObject.ppap) *
-                        1000,
-                ) / 1000
-            }`,
+                    ((jsonObject.ppap + jsonObject.ppapsp) /
+                        (jsonObject.pen +
+                            jsonObject.pineapple +
+                            jsonObject.apple +
+                            jsonObject.ppap +
+                            jsonObject.ppapsp)) *
+                        10000,
+                ) / 10000
+            }%`,
         );
     },
 };

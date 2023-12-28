6a25cbcc90efd93bf2ba6c8510b18caf36116113
sjk <sungntung@gmail.com>
2023-12-28 10:05:16 +0900

    feat(gif): special gifify

--- src/commands/ppap.ts
+++ src/commands/ppap.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { Attachment, AttachmentBuilder, SlashCommandBuilder } from 'discord.js';
 const wait = require('node:timers/promises').setTimeout;
 import fs from 'node:fs';
 import { UpdatedInteraction } from '..';
@@ -13,24 +13,34 @@ module.exports = {
         const jsonObject = JSON.parse(fs.readFileSync('./ppap.json', 'utf-8'));
 
         if (getRandomInt(101) === 100) {
-            jsonObject.s2.ppap = jsonObject.s2.ppap + 1;
-            await interaction.reply({ content: 'Ugh!', fetchReply: true });
-            await wait(1000);
-            await interaction.editReply('Pen-');
-            await wait(220);
-            await interaction.editReply('Pen-Pine');
-            await wait(220);
-            await interaction.editReply('Pen-Pinea');
-            await wait(220);
-            await interaction.editReply('Pen-Pineapple-');
-            await wait(220);
-            await interaction.editReply('Pen-Pineapple-A');
-            await wait(220);
-            await interaction.editReply('Pen-Pineapple-Apple-');
-            await wait(220);
-            await interaction.editReply('Pen-Pineapple-Apple-Pen');
-            await wait(400);
-            await interaction.editReply('**__Pen-Pineapple-Apple-Pen__**');
+            if (getRandomInt(3) === 0) {
+                jsonObject.s2.ppapsp = jsonObject.s2.ppapsp + 1;
+
+                const gif = new AttachmentBuilder('./assets/ppap.gif');
+                const png = new AttachmentBuilder('./assets/ppap.png');
+                const msg = await interaction.reply({ files: [gif] });
+                await wait(3620);
+                await msg.edit({ files: [png] });
+            } else {
+                jsonObject.s2.ppap = jsonObject.s2.ppap + 1;
+                await interaction.reply({ content: 'Ugh!', fetchReply: true });
+                await wait(1000);
+                await interaction.editReply('Pen-');
+                await wait(220);
+                await interaction.editReply('Pen-Pine');
+                await wait(220);
+                await interaction.editReply('Pen-Pinea');
+                await wait(220);
+                await interaction.editReply('Pen-Pineapple-');
+                await wait(220);
+                await interaction.editReply('Pen-Pineapple-A');
+                await wait(220);
+                await interaction.editReply('Pen-Pineapple-Apple-');
+                await wait(220);
+                await interaction.editReply('Pen-Pineapple-Apple-Pen');
+                await wait(400);
+                await interaction.editReply('**__Pen-Pineapple-Apple-Pen__**');
+            }
         } else {
             switch (getRandomInt(3)) {
                 case 0:
